94e2aac2904acd8d4f448f553e85ac72aba306e1
Naufal Abiyyu <naufal1068@gmail.com>
2022-02-17 16:41:00 +0800

    Create Login and dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,23 @@
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:portal_akademik/ui/dashboard.dart';
+import 'package:portal_akademik/ui/login.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: MyApp(),
-    )
-  );
+  runApp(MaterialApp(
+    home: Login(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp>  {
-
-  int currentIndex = 0;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Hello World')
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavyBar(
-        curve: Curves.easeInOutCirc,
-        backgroundColor: Colors.deepOrange,
-        selectedIndex: currentIndex,
-        onItemSelected: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        items: <BottomNavyBarItem>[
-          BottomNavyBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-            activeColor: Colors.white70,
-            inactiveColor: Colors.white
-          ),
-          BottomNavyBarItem(
-              icon: Icon(Icons.people_rounded),
-              title: Text('Presensi'),
-              activeColor: Colors.white70,
-              inactiveColor: Colors.white
-          ),
-          BottomNavyBarItem(
-              icon: Icon(Icons.account_circle_rounded),
-              title: Text('Profile'),
-              activeColor: Colors.white70,
-              inactiveColor: Colors.white
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
---
+++ lib/ui/dashboard.dart
@@ -0,0 +1,128 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+final List<String> imgList = [
+  'assets/images/berakhlak.png',
+  'assets/images/msib.png',
+  'assets/images/permata.png'
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int currentIndex = 0;
+  int _currentIndexSlider = 0;
+  final CarouselController _controller = CarouselController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: imageSliders,
+              carouselController: _controller,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndexSlider = index;
+                    });
+                  }),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    onTap: () => _controller.animateToPage(entry.key),
+                    child: Container(
+                      width: 12.0,
+                      height: 12.0,
+                      margin:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                            .withOpacity(_currentIndexSlider == entry.key ? 0.9 : 0.4),
+                      ),
+                    ),
+                  );
+                }).toList())
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        curve: Curves.easeInOutCirc,
+        backgroundColor: Colors.deepOrange,
+        selectedIndex: currentIndex,
+        onItemSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+              activeColor: Colors.white70,
+              inactiveColor: Colors.white),
+          BottomNavyBarItem(
+              icon: Icon(Icons.people_rounded),
+              title: Text('Presensi'),
+              activeColor: Colors.white70,
+              inactiveColor: Colors.white),
+          BottomNavyBarItem(
+              icon: Icon(Icons.account_circle_rounded),
+              title: Text('Profile'),
+              activeColor: Colors.white70,
+              inactiveColor: Colors.white),
+        ],
+      ),
+    );
+  }
+}
