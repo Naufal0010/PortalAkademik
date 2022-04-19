e4c635000c265ac68ee64458ff7016673b61a5fa
Naufal Abiyyu <naufal1068@gmail.com>
2022-04-19 12:08:06 +0800

    Jadwal hari ini navigate to presensi

--- lib/pages/dashboard/component/jadwal_item_carousel.dart
+++ lib/pages/dashboard/component/jadwal_item_carousel.dart
@@ -7,102 +7,108 @@ import '../../../model/jadwal/model_user_mahasiswa_jadwal_matakuliah.dart';
 
 class JadwalItem extends StatelessWidget {
   final MataKuliah data;
+  final Function() onTap;
 
-  JadwalItem(this.data);
+  JadwalItem(this.data, this.onTap);
 
   @override
   Widget build(BuildContext context) {
     String text = data.jamKuliah;
     var waktuKuliah = text.substring(0, 5);
 
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(4),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Colors.grey.shade300)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 35,
-            backgroundColor: Color(0xffFF9F43),
-            child: CircleAvatar(
-              radius: 30,
-              backgroundColor: Colors.white,
-              child: Text(
-                waktuKuliah,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: ColorPallete.primary),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: Text(
-                      data.mk,
-                      maxLines: 2,
-                      style: TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                        fontSize: 14,
+    return Ink(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.all(4),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.grey.shade300)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 35,
+                backgroundColor: Color(0xffFF9F43),
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    waktuKuliah,
+                    style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: ColorPallete.primary,
-                      ),
-                    ),
+                        fontSize: 18,
+                        color: ColorPallete.primary),
                   ),
-                  Flexible(
-                    child: Row(
-                      children: [
-                        Icon(Icons.location_on_outlined),
-                        SizedBox(
-                          width: 170,
-                          child: Text(
-                            data.ruangKuliah,
-                            maxLines: 2,
-                            style: TextStyle(
-                              overflow: TextOverflow.ellipsis,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                          data.mk,
+                          maxLines: 2,
+                          style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: ColorPallete.primary,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 220,
-                    child: Html(
-                      data: """
-                        ${data.dosen}
-                            """,
-                      style: {
-                        "body": Style(
-                          textOverflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          color: Colors.black,
-                          margin: EdgeInsets.symmetric(horizontal: -10),
-                          fontFamily: 'Poppins',
-                          fontSize: FontSize(14.0),
+                      ),
+                      Flexible(
+                        child: Row(
+                          children: [
+                            Icon(Icons.location_on_outlined),
+                            SizedBox(
+                              width: 170,
+                              child: Text(
+                                data.ruangKuliah,
+                                maxLines: 2,
+                                style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      },
-                    ),
+                      ),
+                      SizedBox(
+                        width: 220,
+                        child: Html(
+                          data: """
+                            ${data.dosen}
+                                """,
+                          style: {
+                            "body": Style(
+                              textOverflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              color: Colors.black,
+                              margin: EdgeInsets.symmetric(horizontal: -10),
+                              fontFamily: 'Poppins',
+                              fontSize: FontSize(14.0),
+                            ),
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -15,6 +15,7 @@ import 'package:portal_akademik/widget/label_sub_header_widget.dart';
 import 'package:portal_akademik/widget/shimmer_widget.dart';
 
 import '../../states/jadwal/state_user_mahasiswa_jadwal_hari_ini.dart';
+import '../presensi/presensi_detail_page.dart';
 
 final List<String> imgList = [
   'assets/images/berakhlak.png',
@@ -65,6 +66,7 @@ class DashboardPage extends StatefulWidget {
 
 class _DashboardPageState extends State<DashboardPage> {
   bool isLoading = false;
+  int _currentIndex = 0;
 
   final CarouselController _controller = CarouselController();
 
@@ -73,7 +75,8 @@ class _DashboardPageState extends State<DashboardPage> {
     UserMahasiswaState userMahasiswa =
         Provider.of<UserMahasiswaState>(context, listen: true);
 
-    UserMahasiswaJadwalHariIniState jadwal = Provider.of<UserMahasiswaJadwalHariIniState>(context, listen: true);
+    UserMahasiswaJadwalHariIniState jadwal =
+        Provider.of<UserMahasiswaJadwalHariIniState>(context, listen: true);
 
     Future<void> refresh() {
       userMahasiswa.refreshData();
@@ -172,7 +175,8 @@ class _DashboardPageState extends State<DashboardPage> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => KalenderAkademikPage()));
+                                  builder: (context) =>
+                                      KalenderAkademikPage()));
                         }),
                     IconButtonCustom(
                         nameLabel: 'Kuesioner',
@@ -219,18 +223,37 @@ class _DashboardPageState extends State<DashboardPage> {
                   height: 150,
                   child: ListView(
                     children: [
-                      jadwal.isLoading ? ShimmerWidget(height: 100, width: 100,) : CarouselSlider(
-                        items: jadwal.data!.data!.map((e) => JadwalItem(e)).toList(),
-                        options: CarouselOptions(
-                          height: 150.0,
-                          enlargeCenterPage: true,
-                          autoPlay: false,
-                          aspectRatio: 16 / 9,
-                          autoPlayCurve: Curves.fastOutSlowIn,
-                          enableInfiniteScroll: false,
-                          viewportFraction: 0.8,
-                        ),
-                      ),
+                      jadwal.isLoading
+                          ? ShimmerWidget(
+                              height: 100,
+                              width: 100,
+                            )
+                          : CarouselSlider(
+                              items: jadwal.data!.data!
+                                  .map(
+                                    (e) => JadwalItem(
+                                      e,
+                                      () {
+                                        Navigator.of(context).push(MaterialPageRoute(
+                                            builder: (context) =>
+                                                PresensiDetailPage(jadwal.data!.data![_currentIndex])));
+                                      },
+                                    ),
+                                  )
+                                  .toList(),
+                              options: CarouselOptions(
+                                height: 150.0,
+                                enlargeCenterPage: true,
+                                autoPlay: false,
+                                aspectRatio: 16 / 9,
+                                autoPlayCurve: Curves.fastOutSlowIn,
+                                enableInfiniteScroll: false,
+                                viewportFraction: 0.8,
+                                onPageChanged: (index, _) {
+                                  _currentIndex = index;
+                                }
+                              ),
+                            ),
                     ],
                   ),
                 ),
@@ -247,8 +270,7 @@ class _DashboardPageState extends State<DashboardPage> {
                           enlargeCenterPage: true,
                           aspectRatio: 2.0,
                           onPageChanged: (index, reason) {
-                            setState(() {
-                            });
+                            setState(() {});
                           }),
                     )
                   ],
