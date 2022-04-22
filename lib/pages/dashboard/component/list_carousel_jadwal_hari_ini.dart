949a0e5a84bbab01566454500d15a89d342d7c6c
Naufal Abiyyu <naufal1068@gmail.com>
2022-04-22 13:50:31 +0800

    Change size for list_carousel_jadwal_hari_ini.dart

--- lib/pages/dashboard/component/list_carousel_jadwal_hari_ini.dart
+++ lib/pages/dashboard/component/list_carousel_jadwal_hari_ini.dart
@@ -20,35 +20,25 @@ Widget ListCarouselJadwalHariIni(
         backgroundColor: Colors.grey,
         textColor: Colors.white,
         fontSize: 16.0);
-
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          state.refreshData();
-        },
-        child: Center(child: Text('Refresh')),
-        style: ElevatedButton.styleFrom(
-            primary: ColorPallete.primary, onPrimary: Colors.white),
-      ),
-    );
   }
 
   if (state.data?.data?.length == 0) {
     return Container(
-        width: MediaQuery.of(context).size.width,
-        height: 100,
-        padding: EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          border: Border.all(width: 1, color: ColorPallete.primary),
-          // color: Colors.amber,
+      width: MediaQuery.of(context).size.width,
+      height: 150,
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(width: 1, color: ColorPallete.primary),
+        // color: Colors.amber,
+      ),
+      child: Center(
+        child: Text(
+          'Tidak ada jadwal mata kuliah hari ini',
+          style: TextStyle(color: Colors.black),
         ),
-        child: Center(
-          child: Text(
-            'Tidak ada jadwal mata kuliah hari ini',
-            style: TextStyle(color: Colors.black),
-          ),
-        ));
+      ),
+    );
   }
 
   return CarouselSlider(
@@ -65,15 +55,16 @@ Widget ListCarouselJadwalHariIni(
         )
         .toList(),
     options: CarouselOptions(
-        height: 150.0,
-        enlargeCenterPage: true,
-        autoPlay: false,
-        aspectRatio: 16 / 9,
-        autoPlayCurve: Curves.fastOutSlowIn,
-        enableInfiniteScroll: false,
-        viewportFraction: 0.8,
-        onPageChanged: (index, _) {
-          _currentIndex = index;
-        }),
+      height: 150.0,
+      enlargeCenterPage: true,
+      autoPlay: false,
+      aspectRatio: 16 / 9,
+      autoPlayCurve: Curves.fastOutSlowIn,
+      enableInfiniteScroll: false,
+      viewportFraction: 0.8,
+      onPageChanged: (index, _) {
+        _currentIndex = index;
+      },
+    ),
   );
 }
