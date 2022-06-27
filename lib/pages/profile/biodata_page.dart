e9d51def1b27caa221872c131cba7ed12afd7e33
Nanda <ananda.noorrizki@ulm.ac.id>
2022-06-27 10:51:23 +0800

    biodata

--- lib/pages/profile/biodata_page.dart
+++ lib/pages/profile/biodata_page.dart
@@ -3,7 +3,7 @@ import 'package:portal_akademik/pages/profile/component/biodata_diri.dart';
 import 'package:portal_akademik/pages/profile/component/biodata_header.dart';
 import 'package:portal_akademik/pages/profile/component/biodata_orangTua.dart';
 import 'package:portal_akademik/pages/profile/component/biodata_riwayatPendidikan.dart';
-import 'package:portal_akademik/pages/profile/subpages/biodata_edit.dart';
+import 'package:portal_akademik/pages/profile/subpages/biodataEdit_page.dart';
 import 'package:portal_akademik/states/state.dart';
 import 'package:portal_akademik/states/state_user_mahasiswa_profil_editable.dart';
 import 'package:portal_akademik/widget/label_sub_header_widget.dart';
@@ -27,29 +27,34 @@ class BiodataMenuPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => BiodataEditPage()));
-                },
-                icon: const Icon(Icons.edit)),
-          ],
+          title: Text('Biodata',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+              overflow: TextOverflow.ellipsis),
+          // actions: [
+          //   IconButton(
+          //       onPressed: () {
+          //         Navigator.push(
+          //             context,
+          //             MaterialPageRoute(
+          //                 builder: (context) => BiodataEditPage()));
+          //       },
+          //       icon: const Icon(Icons.edit)),
+          // ],
           backgroundColor: Colors.white,
-          foregroundColor: const Color(0xff4848),
+          // foregroundColor: const Color(0xff4848),
           elevation: 0,
           iconTheme: IconThemeData(color: Colors.black),
-          centerTitle: false),
-      backgroundColor: Colors.white,
+          centerTitle: true),
       body: SafeArea(
         child: RefreshIndicator(
           onRefresh: refresh,
           child: SingleChildScrollView(
             physics: AlwaysScrollableScrollPhysics(),
             child: Padding(
-              padding: const EdgeInsets.all(24.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
